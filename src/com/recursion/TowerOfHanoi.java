a3f21fcc691080d5dd654b3ba2725ffb07664628
Basanth215 <basanth.ies@gmail.com>
2021-10-28 14:36:17 +0530

    write meaningful code

--- src/com/recursion/TowerOfHanoi.java
+++ src/com/recursion/TowerOfHanoi.java
@@ -1,18 +1,26 @@
 package com.recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TowerOfHanoi {
     public static void main(String...args){
         int noOfRings = 3;//The logic will work for any no of rings
-        playTOH("A", "C", "B", noOfRings);
+        List<String> statements = new ArrayList<>();
+        playTOH("A", "C", "B", noOfRings, statements);
+        statements.stream().forEach(statement -> {
+            String[] split = statement.split(",");
+            System.out.println("from: "+split[0]+" to: "+split[1]+" ring: "+split[2]);
+        });
     }
 
-    private static void playTOH(String A, String C, String B, int ring) {
+    private static void playTOH(String A, String C, String B, int ring, List<String> statements) {
         if (ring==1){
-            System.out.println("From: "+A+" to: "+C+" ring: "+ring);
+            statements.add(A+","+C+","+ring);
         } else {
-            playTOH(A, B, C, ring-1);
-            System.out.println("From: "+A+" to: "+C+" ring: "+ring);
-            playTOH(B, C, A, ring-1);
+            playTOH(A, B, C, ring-1, statements);
+            statements.add(A+","+C+","+ring);
+            playTOH(B, C, A, ring-1, statements);
         }
     }
 }
